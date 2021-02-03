491ef60787c2be31e4b3306d45c408d2d5dbdbe8
coderfoodie <lsomodio@gmail.com>
2021-02-03 15:11:31 +0800

    3d animation

---
+++ app.js
@@ -0,0 +1,32 @@
+const card = document.querySelector(".card")
+const container = document.querySelector(".container")
+const title = document.querySelector(".title")
+const shoe = document.querySelector(".shoe img")
+const purchase = document.querySelector(".purchase")
+const desc = document.querySelector(".info h3")
+const sizes = document.querySelector(".sizes")
+
+container.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25
+    let yAxis = (window.innerHeight /2 - e.pageY) / 25
+    card.style.transform = `rotateY($(xAxis)deg rotatrX($(yAxis)deg)`
+})
+
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none"
+    title.style.transform = "translateZ(150px)"
+    shoe.style.transform = "translateZ(200px) rotateZ(-45deg)"
+    desc.style.transform = "translateZ(125px)"
+    sizes.style.transform = "translateZ(100px)"
+    purchase.style.transform = "translateZ(75px)"
+})
+
+container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease"
+    card.style.transform = "rotateY(0deg) rotateX(0deg)"
+    title.style.transform = "translateZ(0px)"
+    shoe.style.transform = "translateZ(0px) rotateZ(0deg)"
+    desc.style.transform = "translateZ(0px)"
+    sizes.style.transform = "translateZ(0px)"
+    purchase.style.transform = "translateZ(0px)"
+})
